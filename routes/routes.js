15ab8468269b3c1c8254d6d74b1b32ea67f0a42f
Basel <basil.omsha@student.hamk.fi>
2023-04-25 11:13:18 +0300

    read all posts

--- routes/routes.js
+++ routes/routes.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 const loginController = require('../controllers/login.js');
 const signupController = require('../controllers/signup.js');
+const readController = require("../controllers/read.js");
+
 
 router.get('/', loginController.home);
 
 router.get('/registration', signupController.signupForm);
 
 router.post('/signup', signupController.signup);
+
+router.get("/guest",readController.guestPage);
 
 module.exports = router;
