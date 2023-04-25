ce959a0bd9b689371b249a1dfc4d9477600de439
Janica Narhi <nieminenjanica@gmail.com>
2023-04-25 11:55:11 +0300

    changes

--- controllers/login.js
+++ controllers/login.js
@@ -5,13 +5,10 @@ const router = express.Router();
 
 const User = require('../models/User');
 
-const dbURI = 'mongodb+srv://'+ process.env.DBUSER +':'+ process.env.DBPASSWD +''+ process.env.CLUSTER +'.mongodb.net/'+ process.env.DB +'?retryWrites=true&w=majority'
-mongoose.connect(dbURI); 
-const Product = require('../models/User');
+
 
 //home/root
 const home = async (req,res) => {
-    // res.send('My MVC App');
     res.render('logIn', {
         // page_title: "Library",
         // content: data
